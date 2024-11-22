1ca4946f30515dc9575c8b47d317e5a0e7994751
Ryan <eldridgerdev@gmail.com>
2024-11-21 20:35:21 -0500

    Start making Header look better

--- components/custom/navigation/top-navigation.tsx
+++ components/custom/navigation/top-navigation.tsx
@@ -1,30 +1,77 @@
+"use client";
 import {
   NavigationMenu,
   NavigationMenuLink,
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
 import { NavigationMenuItem } from "@radix-ui/react-navigation-menu";
 import Link from "next/link";
+import { useState, useEffect, HTMLAttributes } from "react";
 
-const headerClasses =
-  "sticky top-0 z-50 w-full border-b-8 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border";
+interface NavLink {
+  title: string;
+  href: string;
+}
+const navLinks: NavLink[] = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Test1",
+    href: "/",
+  },
+  {
+    title: "Test2",
+    href: "/",
+  },
+  {
+    title: "Test3",
+    href: "/",
+  },
+];
+
+const Logo = (props: HTMLAttributes<HTMLDivElement>) => (
+  <Link href="/" className={`flex items-center space-x-2 ${props.className}`}>
+    <span className="text-xl font-bold">Logo</span>
+  </Link>
+);
 export default function TopNav() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className={headerClasses}>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href={"/"} legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Test
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-      <div className="flex h-14 max-w-screen-2xl mx-auto justify-center ">
-        <div className="py-4 mx-4 items-center"></div>
+    <header
+      className={cn(
+        "sticky top-0 z-50 w-full borderb bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60",
+        isScrolled && "shadow-sm",
+      )}
+    >
+      <div className="container flex h-16 items-center">
+        <Logo className="mx-8" />
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href={"/"} legacyBehavior passHref>
+                <NavigationMenuLink
+                  className={navigationMenuTriggerStyle()}
+                ></NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
     </header>
   );
