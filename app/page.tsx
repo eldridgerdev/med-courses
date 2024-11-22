c04877803db2f07f7845fcd73eb52d30941c93be
Ryan <eldridgerdev@gmail.com>
2024-11-22 17:41:47 -0500

    Move topnav to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
-import TopNav from "@/components/custom/navigation/top-navigation";
-import Image from "next/image";
 import testData from "@/testData/medbridge-courses.json";
 import CourseCard from "./courses/CourseCard";
 
 export default function Home() {
   return (
     <>
-      <TopNav />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col row-start-2 items-center sm:items-start rounded-[0.5rem] border bg-background shadow w-full">
           <div
