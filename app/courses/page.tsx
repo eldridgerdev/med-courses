da0be08a6eb6f71ecc3577d71af69634d1c6e979
Ryan <eldridgerdev@gmail.com>
2024-11-22 17:44:50 -0500

    Move courses to courses page

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import testData from "@/testData/medbridge-courses.json";
-import CourseCard from "./courses/CourseCard";
+import CourseCard from "./CourseCard";
 
 export default function Home() {
   return (
@@ -9,7 +9,12 @@ export default function Home() {
         <main className="flex flex-col row-start-2 items-center sm:items-start rounded-[0.5rem] border bg-background shadow w-full">
           <div
             className={`w-full items-start justify-center gap-10 rounded-lg p-8 flex flex-wrap flex-row flex-basis-0 grow-0 bg-bgp`}
-          ></div>
+          >
+            {/*<div className="items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">*/}
+            {testData.content.map((data) => (
+              <CourseCard key={data.title} data={data} />
+            ))}
+          </div>
         </main>
       </div>
     </>
