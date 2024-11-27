8dacf326d89c033bce37b63d88448c095875da8b
Ryan <eldridgerdev@gmail.com>
2024-11-27 11:01:14 -0500

    landing cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@
 
 import CTA from "@/components/custom/ui/CTA";
 import Hero from "@/components/custom/ui/Hero";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  // @TODO: Temporary until landing page is built
-  // redirect("/courses");
   return (
     <main className="flex-1">
       <Hero />
